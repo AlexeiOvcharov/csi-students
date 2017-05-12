baee46da37988ec8f2895434802ae7c89547cb2a
Alexei <senserlex@gmail.com>
2017-05-13 01:14:39 +0300

    Complete course work.

--- kurs/calc.m
+++ kurs/calc.m
@@ -1,8 +1,7 @@
-clear
 %% Initial data
 K = 210;
-T1 = 1;
-T2 = 2;
+T1 = 0.04;
+T2 = 0.2;
 tp = 0.1;
 sigma = 27;
 gMax = 5;
@@ -10,20 +9,26 @@ g0Max = 0.8;
 eMax = 0.015;
 
 %% From table
-L1 = 19; L2 = -19;
+L1 = 18; L2 = -18;
 wc = 4*pi/tp*0.9;
 
 %% Calculate desired params
-w0 = gMax/g0Max;
-accMax = g0Max*w0^2;
-Kacc = accMax/eMax;
-
-w1 = Kacc/Kv;
-w2 = sqrt(Kv*w1/10^(L1/20));
-w3 = wc/10^(L2/20);
-
-%% Second calc
 Kv = gMax/eMax;
-w2 = wc/10^(L1/20);
+w0 = gMax/g0Max;
+Kd = Kv*w0*T2;
+
+w1 = 1/T2;
+w2 = sqrt(Kd*w1^2/wc);
 w3 = wc/10^(L2/20);
-w1 = sqrt(wc/Kv)*w2;
+
+%% Calc
+test = [
+    1, 20*log10(Kd);
+    w1, 20*log10(Kd/w1);
+    w2, 20*log10(Kd*w1^2/w2^3);
+    w2, 20*log10(wc/w2);
+    w3, 20*log10(wc/w3);
+];
+
+%% Write data to file
+csvwrite('data/Sin.csv', [simout.Time, simout.Data]);
